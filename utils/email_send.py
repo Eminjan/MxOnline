39f88023ffb0bc718355ff4a358a8c24e49a9786
Eminjan <eminjan123@aliyun.com>
2018-12-12 14:34:19 +0800

    添加邮箱验证码HTML格式

--- utils/email_send.py
+++ utils/email_send.py
@@ -3,10 +3,11 @@
 #Author:Eminjan
 #@Time  :2018/4/18 20:06
 from random import Random
-from django.core.mail import send_mail
+from django.core.mail import send_mail,EmailMessage
 
 from users.models import EmailVerifyRecord
 from Mxonline.settings import EMAIL_FROM
+from django.template import loader
 
 def random_str(random_length=8):
     str = ''
@@ -36,22 +37,48 @@ def send_register_email(email,send_type='register'):
 
     if send_type == 'register':
         email_title = 'Mx Online 激活链接'
-        email_body = '请点击下面的链接激活你的账号：http://127.0.0.1:8000/active/{0}'.format(code)
+        # email_body = '请点击下面的链接激活你的账号：http://127.0.0.1:8000/active/{0}'.format(code)
+        email_body = loader.render_to_string(
+            "email_register.html",  # 需要渲染的html模板
+            {
+                "active_code": code  # 参数
+            }
+        )
 
-        send_status =  send_mail(email_title,email_body,EMAIL_FROM,[email])
+        msg = EmailMessage(email_title, email_body, EMAIL_FROM, [email])
+        msg.content_subtype = "html"
+        send_status = msg.send()
+        # send_status =  send_mail(email_title,email_body,EMAIL_FROM,[email])
         if send_status:
             pass
     elif send_type == 'forget':
         email_title = 'Mx Online 密码重置链接'
-        email_body = '请点击下面的重置你的密码：http://127.0.0.1:8000/reset/{0}'.format(code)
+        # email_body = '请点击下面的重置你的密码：http://127.0.0.1:8000/reset/{0}'.format(code)
+        email_body = loader.render_to_string(
+            "email_forget.html",  # 需要渲染的html模板
+            {
+                "active_code": code  # 参数
+            }
+        )
 
-        send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
+        msg = EmailMessage(email_title, email_body, EMAIL_FROM, [email])
+        msg.content_subtype = "html"
+        send_status = msg.send()
+        # 如果发送成功
         if send_status:
             pass
     elif send_type =='update_email':
         email_title = 'Mx Online 邮箱修改验证码'
-        email_body = '你的邮箱验证码为：{0}'.format(code)
+        # email_body = '你的邮箱验证码为：{0}'.format(code)
+        email_body = loader.render_to_string(
+            "email_update_email.html",  # 需要渲染的html模板
+            {
+                "active_code": code  # 参数
+            }
+        )
 
-        send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
+        msg = EmailMessage(email_title, email_body, EMAIL_FROM, [email])
+        msg.content_subtype = "html"
+        send_status = msg.send()
         if send_status:
             pass
